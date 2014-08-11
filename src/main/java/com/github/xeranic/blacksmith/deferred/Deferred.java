5fb9ee967c88487eaf06a177b875aa67efe05609
Wang <xeranic@gmail.com>
2014-08-11 11:20:58 -0700

    fix code style

--- src/main/java/com/github/xeranic/blacksmith/deferred/Deferred.java
+++ src/main/java/com/github/xeranic/blacksmith/deferred/Deferred.java
@@ -5,11 +5,11 @@ public class Deferred<T> extends AbstractPromise<T> {
 	public static <T> Deferred<T> createResolved(T result) {
 		return new Deferred<T>().resolve(result);
 	}
-	
+
 	public static <T> Deferred<T> createRejected(Throwable failure) {
 		return new Deferred<T>().reject(failure);
 	}
-		
+
 	public final synchronized Deferred<T> resolve(T result) {
 		if (!isPending()) {
 			throw new IllegalStateException("already resolved or rejected");
@@ -32,14 +32,14 @@ public class Deferred<T> extends AbstractPromise<T> {
 		return this;
 	}
 
-    public final synchronized Deferred<T> notify(Progress progress) {
-    	if (!isPending()) {
-    		throw new IllegalStateException("already resolved or rejected");
-    	}
-    	this.progress = progress;
-    	triggerProgress();
-    	return this;
-    }
+	public final synchronized Deferred<T> notify(Progress progress) {
+		if (!isPending()) {
+			throw new IllegalStateException("already resolved or rejected");
+		}
+		this.progress = progress;
+		triggerProgress();
+		return this;
+	}
 
 	public final Promise<T> promise() {
 		return this;
